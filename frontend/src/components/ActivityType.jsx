494de014ee46f4d8d5d415721830e7cfd5b4e0ff
Yuhan Li <yuhanl@student.chalmers.se>
2025-02-10 21:24:12 +0100

    Initially complete the profile page

---
+++ frontend/src/components/ActivityType.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function FreeSolo() {
+  return (
+    <Stack spacing={2} sx={{ width: "100%", minWidth: 215 }}>
+      <Autocomplete
+        freeSolo
+        id="activity-type"
+        disableClearable
+        options={activityType.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search/Select the Activity Type"
+            slotProps={{
+              input: {
+                ...params.InputProps,
+                type: 'search',
+              },
+            }}
+          />
+        )}
+      />
+    </Stack>
+  );
+}
+
+const activityType = [
+    { title: 'Yoga' },
+    { title: 'Pilates' },
+    { title: 'Jogging' },
+    { title: 'Swimming' },
+    { title: 'Cycling' },
+    { title: 'Jump Rope' },
+    { title: 'No Equipment Exercise' },
+];
---
+++ frontend/src/components/ExerciseLevel.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function FreeSolo() {
+  return (
+    <Stack spacing={2} sx={{ width: "80%", minWidth: 215 }}>
+      <Autocomplete
+        freeSolo
+        id="exeerise-level"
+        disableClearable
+        options={exerciseLevel.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search/Select your Level"
+            slotProps={{
+              input: {
+                ...params.InputProps,
+                type: 'search',
+              },
+            }}
+          />
+        )}
+      />
+    </Stack>
+  );
+}
+
+const exerciseLevel = [
+    { title: 'Beginer' },
+    { title: 'Intermediate' },
+    { title: 'Advanced' },
+    { title: 'Expert' },
+    { title: 'Professional' },
+
+];
